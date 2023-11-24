977759d0f3b71742074fd03f96e4fb20cb931114
MaxamedAweis90 <126536619+MaxamedAweis90@users.noreply.github.com>
2023-11-24 10:02:25 +0300

    Add files via upload

---
+++ My profolio/My profolio/Test.py
@@ -0,0 +1,13 @@
+# x=100
+# if(x>100):
+#     y=30
+#     z=50
+#     print(y,z)
+
+# for numbers in range(0,1001):
+#     print(f"{numbers}")
+
+total = 0
+for count in range(1,6):
+    total = total + count
+    print(total)
